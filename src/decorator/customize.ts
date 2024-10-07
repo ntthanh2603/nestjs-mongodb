66341f0c39d168704792592a6dddb67207cd5a2d
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2024-10-07 16:25:22 +0700

    refactor

--- src/decorator/customize.ts
+++ src/decorator/customize.ts
@@ -4,6 +4,7 @@ export const IS_PUBLIC_KEY = "isPublic";
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 
+// Interceptor
 export const RESPONSE_MESSAGE = 'response_message';
 export const ResponseMessage = (message: string) =>
     SetMetadata(RESPONSE_MESSAGE, message);
